561ef8626402632ac52589ccfb18c1d452d23537
Nazareno Fioretti <fiorettinazareno@gmail.com>
2024-12-28 19:38:30 -0300

    Terminando la calculadora

--- Stand/calculadora.cpp
+++ Stand/calculadora.cpp
@@ -3,34 +3,69 @@ using namespace std;
 #include <string>
 
 int main() {
-	int num1;
-	int num2;
-	string selection;
-	cout << "Hola! Seleccione la operacion que desea llevar a cabo!" << endl;
+
+	int out = 0;
+
+	while(out == 0){
 	
-	cout << "1) Suma\n" << "2) Resta\n" << "3) Multiplicacion\n" << "4) Division\n";
-	cin >> selection;
-	cout << selection << endl;
-	if(selection != "1" && selection != "2" && selection != "3" && selection != "4"){
-		cout << "La opcion no coincide con ninguna listada. Intentelo de nuevo" << endl;
+		int num1;
+		int num2;
+		string selection;
+		cout << "Hola! Seleccione la operacion que desea llevar a cabo!" << endl;
+	
+		cout << "1) Suma\n" << "2) Resta\n" << "3) Multiplicacion\n" << "4) Division\n";
+		cin >> selection;
 
+		if(selection != "1" && selection != "2" && selection != "3" && selection != "4"){
+			cout << "La opcion no coincide con ninguna listada. Intentelo de nuevo" << endl;
 
-	} else { 
+		} else { 
 
-	if (selection == "1"){
-		cout << "Primer numero: "; 
-		cin >> num1;
+		if (selection == "1"){
+			cout << "Primer numero: "; 
+			cin >> num1;
 
-		cout << "Segundo numero: ";
-		cin >> num2; 
+			cout << "Segundo numero: ";
+			cin >> num2; 
 
-		cout << "Resultado: " << num1 + num2 << endl;
-	} 
-	if (selection == "2"){
-		cout << "Primer numero: ";
+			cout << "Resultado: " << num1 + num2 << endl;
+		} 
+		if (selection == "2"){
+			cout << "Primer numero: ";
+			cin >> num1; 
 
+			cout << "Segundo numero: ";
+			cin >> num2; 
+
+			cout << "Resultado: " << num1 - num2 << endl;  
+		} 
+		if (selection == "3"){
+			cout << "Primer numero: "; 
+			cin >> num1; 
+
+			cout << "Segundo numero: ";
+			cin >> num2; 
+
+			cout << "Resultado: " << num1 * num2 << endl;
+		}
+		if (selection == "4"){
+			cout << "Primer numero: ";
+			cin >> num1;
+
+			cout << "Segundo numero (Divisor): "; 
+			cin >> num2; 
+
+			if (num2 == 0){
+		 		cout << "No se puede dividir por cero, ingrese otro numero" << endl;	
+			} else {
+				cout << "Resultado: " << num1 / num2 << endl;
+			}
+		}
+	
 	}
-	}	
-	return 0;
+		cout << "Quiere hacer otra operacion? (0 para si, otro numero para no)" << endl;
+		cin >> out;
+	}
+		return 0;
 
 }
