32312a049d240a16e3e79babaced5247b63bfd23
Nazareno Fioretti <fiorettinazareno@gmail.com>
2024-12-31 21:04:45 -0300

    Adivinar numero, programa choto

---
+++ Stand/adivinarnumero.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	cout << "Hola! A continuacion, se elegira un numero al azar entre 1 y 10, adivinelo!" << endl; 
+	int randomNum = rand() % 11; 	
+	int guess; 	
+	
+	while(randomNum != guess){
+		cout << "Adivine: ";
+		cin >> guess;
+		if(guess == randomNum){
+			cout << "Enhorabuena, ha adivinado, era: " << randomNum << endl;
+			break;
+
+		} else {
+			cout << "No es ese, vuelva a intentarlo!" << endl;	
+			continue;	
+	
+		}
+
+	
+	
+	}	
+	return 0; }
