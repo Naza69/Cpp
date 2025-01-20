c2bdb691f12f380a89b280209c8e3fff265b36af
Nazareno Fioretti <fiorettinazareno@gmail.com>
2025-01-20 01:08:55 -0300

    Extiendo la matriz y mejoro el efecto de superposicion

--- stand/matrizflashera.cpp
+++ stand/matrizflashera.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib> 
 #include <unistd.h>
 #include <format>
+#include <string>
 using namespace std; 
 
 
@@ -11,8 +12,8 @@ void clearConsole() {
 }
 
 int main(){
-	int out = 0;
-	while(out != 1){
+	string out = "0";
+	while(out != "1"){
 	
 		int time; 
 		cout << "Eliga la cantidad de tiempo que desea ver la matriz flashera (Segundos): ";		
@@ -20,29 +21,41 @@ int main(){
 		int counter = time;	
 		for(int a = 1; a <= 11*time; a++){
 
-			int matrix[5][5];
-			for(int i = 0; i < 5; i++){
-				for(int j = 0;j < 5; j++){
+			int matrix[20][20];
+			for(int i = 0; i < 20; i++){
+				for(int j = 0;j < 20; j++){
 					matrix[i][j] = rand() % 10;	
 			
 				}
 			}	
 
-			clearConsole();
-			
-			for(int i = 0; i < 5; i++){
-				for(int j = 0; j < 5; j++){
+			//clearConsole();
+			//system("clear");
+				
+
+			for(int i = 0; i < 20; i++){
+				for(int j = 0; j < 20; j++){
 					cout <<  "[" << matrix[i][j] << "]";	
 				
 				}
 				cout << "" << endl;
 			};
+
+			for(int i = 0; i < 20; i++){
+				cout << "\033[F";	
+			
+			}
+			
+					
 			usleep(90000);
+
+			
 		}
+		system("clear");
 		cout << "" << endl;
 		cout << "Quiere volver a repetir el proceso? (1 para no, 0 para si)" << endl;
 		cin >> out;
-		while(out != 1 && out != 0){
+		while(out != "1" && out != "0"){
 			cout << "ERROR: Solo puede ingresar 0 o 1 (1 para salir, 0 para volver a repetir el proceso)" << endl;
 			cin >> out;	
 	
