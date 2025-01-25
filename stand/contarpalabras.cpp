cf03e9a5397807efb6ad2caa9eafe0224482498a
Nazareno Fioretti <fiorettinazareno@gmail.com>
2025-01-25 20:42:13 -0300

    Contador de palabras de cadena ingresada por usuario con un error donde espacio final es contado

---
+++ stand/contarpalabras.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string> 
+#include <cstring>
+using namespace std;
+
+int main(){
+	string sentence; 
+	int counter = 0;
+	cout << "A continuacion, ingrese una cadena:" << endl;
+	getline(cin, sentence);
+	string emptysentence = "";
+
+	for(int i = 0; i < sentence.length(); i++){
+		if(sentence[i] == ' ' && i + 1 < sentence.length() && sentence[i+1] != ' '){
+			counter++;	
+		
+		}			
+
+
+	}	
+	if(!sentence.empty() && sentence[0] != ' '){
+		counter++;
+	}
+	cout << "La cadena ingresada tiene un total de " << counter << " palabras." << endl;
+	
+	return 0;	
+
+}
